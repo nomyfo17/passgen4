3af195a69ea1625ad1af7ec994ebb787522e9901
nomyfo <nomyfo171234@gmail.com>
2026-08-10 21:27:59 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'passgen-flat-v1';
+const ASSETS = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'styles.css',
+  'generator.js',
+  'storage.js',
+  'app.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
